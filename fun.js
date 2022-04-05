9e3143e8580ce91bca7a6e834cf88d903a3fa629
shhady <shhadyse@gmail.com>
2022-04-05 23:42:12 +0300

    functionbasic

---
+++ fun.js
@@ -0,0 +1,7 @@
+function describeCountry(country, population, capital){
+    console.log(`${country} has ${population} million people and its capital is ${capital}`);
+}   
+
+describeCountry('Finland', 6, 'Helsinky');
+describeCountry('England', 50, 'London');
+describeCountry('Greece', 9, 'Athens');
